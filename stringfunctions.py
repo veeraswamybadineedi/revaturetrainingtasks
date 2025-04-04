886eb498858319de7bef9cd22637afdd18d986e0
veeraswamybadineedi <127339198+veeraswamybadineedi@users.noreply.github.com>
2025-04-04 09:01:28 +0530

    conversion funstions

---
+++ stringfunctions.py
@@ -0,0 +1,20 @@
+name="Veera Swamy"
+# case conversion
+
+# upper() function is used to convert all characters into upper case
+print(name.upper()) #VEERA SWAMY
+
+# lower() function is used to convert all characters into lower case
+print(name.lower()) #veera swamy
+
+# title() function is used to convert each word first character into upper case
+print("hi hello good morning".title()) #Hi Hello Good Morning
+
+# swapcase() function is used to convert all small characters into capital characters vice-versa
+print(name.swapcase()) #vEERA sWAMY
+
+# capitalize() function is used for converting the first character into the capital letter
+print("hi hello good morning".capitalize()) #Hi hello good morning
+
+# len() function is used to find length of the string
+print("length of the name",len(name))
